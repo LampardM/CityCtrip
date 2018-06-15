917863c7112050335784ef220b6bc536917682a8
zlong <zlong@Ctrip.com>
2018-06-15 15:45:32 +0800

    components

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,11 +6,21 @@
 
 <script>
 import SearchInput from '@/components/SearchInput'
+import Scroll from '@/components/Scroll'
+import CityList from '@/components/CityList'
+import CityItem from '@/components/CityItem'
+import PositionBox from '@/components/PositionBox'
+import NavList from '@/components/NavList'
 
 export default {
   name: "Index",
   components: {
-    SearchInput
+    SearchInput,
+    Scroll,
+    CityList,
+    CityItem,
+    PositionBox,
+    NavList
   },
   data() {
     return {
