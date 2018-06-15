77d45ef1280767989082a53909d3e3cbf1700f7b
zlong <zlong@Ctrip.com>
2018-06-15 14:40:17 +0800

    'CityCtrip'

---
+++ src/pages/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="select-box">
+    <search-input></search-input>
+  </div>
+</template>
+
+<script>
+import SearchInput from '@/components/SearchInput'
+
+export default {
+  name: "Index",
+  components: {
+    SearchInput
+  },
+  data() {
+    return {
+      
+    };
+  },
+  mounted() {
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
